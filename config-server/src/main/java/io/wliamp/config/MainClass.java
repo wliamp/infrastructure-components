47ab3e86aac7e8a987e282d92758917842c3975a
William Phan <phnam230197@gmail.com>
2025-08-22 17:55:21 +0700

    chore(config): update config, code cleanup

--- config-server/src/main/java/io/wliamp/config/MainClass.java
+++ config-server/src/main/java/io/wliamp/config/MainClass.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class MainClass {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MainClass.class, args);
 	}
-
 }
