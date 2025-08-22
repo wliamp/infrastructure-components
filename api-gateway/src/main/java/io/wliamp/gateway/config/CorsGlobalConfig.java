79d94c1ccc6a6f9fa0f249b63b844842aa873367
William Phan <phnam230197@gmail.com>
2025-08-23 00:54:23 +0700

    refactor(gateway): change properties binding method

--- api-gateway/src/main/java/io/wliamp/gateway/config/CorsGlobalConfig.java
+++ api-gateway/src/main/java/io/wliamp/gateway/config/CorsGlobalConfig.java
@@ -1,24 +1,28 @@
 package io.wliamp.gateway.config;
 
+import io.wliamp.gateway.compo.CorsProperties;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Slf4j
 @Configuration
+@EnableConfigurationProperties(CorsProperties.class)
+@RequiredArgsConstructor
 public class CorsGlobalConfig implements WebFluxConfigurer {
-    @Value("${cors.allowed-origins}")
-    private String[] allowedOrigins;
+    private final CorsProperties corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String[] originsArray = corsProperties.getAllowedOrigins().toArray(new String[0]);
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins(originsArray)
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-        log.info("CORS configuration applied. Allowed origins: {}", (Object) allowedOrigins);
+        log.info("CORS configuration applied. Allowed origins: {}", (Object) originsArray);
     }
 }
