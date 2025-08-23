7b4bcccc46174c4de5774249fbe04485ca552ed6
William Phan <phnam230197@gmail.com>
2025-08-24 02:00:25 +0700

    feat(repo): initial module authentication

---
+++ authentication/src/test/java/authn/TestClass.java
@@ -0,0 +1,13 @@
+package authn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestClass {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
